efc09a434f37b466adb6712d4763d4f3397c8176
Saemi Jung <jung.sammie@gmail.com>
2022-11-22 14:19:32 +1300

    Modal implementation

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -7,16 +7,41 @@ import suggestNLImg from "@assets/peep-2.png";
 import Image from "next/image";
 import Btn from "./Btn";
 
-const Modal = () => {
+const Modal = ({ showModal, onClose }) => {
   const [isBrowser, setIsBrowser] = useState(false);
+  const [isA, setIsA] = useState(false);
+  const [isB, setIsB] = useState(false);
 
   useEffect(() => {
-    setIsBrowser(false); // true
+    setIsBrowser(true); // true
   }, []);
 
-  const modalContent = (
-    <div className="fixed top-0 left-0 w-full h-screen z-50 bg-[rgba(0,0,0,0.5)] flex justify-center items-center">
-      <div className="w-[600px] py-8 px-[36px] bg-gray-50 rounded-2xl">
+  const handleClose = () => {
+    onClose();
+    setIsA(false);
+    setIsB(false);
+  };
+
+  const handleCardClick = (e) => {
+    const cardId = e.target.id;
+    if (cardId === "suggestNewleka" && !isB) {
+      setIsA(true);
+    } else if (cardId === "suggestNewsletter" && !isA) {
+      setIsB(true);
+    } else if (cardId === "suggestNewleka") {
+      let setClicked = !isA;
+      setIsA(setClicked);
+      setIsB(!setClicked);
+    } else if (cardId === "suggestNewsletter") {
+      let setClicked = !isB;
+      setIsA(!setClicked);
+      setIsB(setClicked);
+    }
+  };
+
+  const modalContent = showModal ? (
+    <div className="fixed top-0 left-0 w-full h-screen z-[9999] bg-[rgba(0,0,0,0.5)] flex justify-center items-center">
+      <div className="w-[80vw] lg:w-[600px] py-8 px-[36px] bg-gray-50 rounded-2xl">
         <div>
           <h3 className="text-gray-600 font-bold text-2xl leading-loose">
             제안하기
@@ -27,10 +52,14 @@ const Modal = () => {
           </p>
         </div>
         <div className="flex justify-center items-center">
-          <button>
+          <button onClick={handleCardClick}>
             <div
               id="suggestNewleka"
-              class="flex justify-center items-center flex-col w-[250px] h-[336px] bg-white rounded-lg p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+              className={
+                isA
+                  ? "bg-primary-100 border-2 border-primary-450 flex justify-center items-center flex-col  w-[30vw] lg:w-[250px] h-[336px] rounded-lg lg:p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+                  : "flex justify-center items-center flex-col w-[30vw] lg:w-[250px] h-[336px] bg-white rounded-lg lg:p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+              }
             >
               <Image
                 id="suggestNewleka"
@@ -38,39 +67,49 @@ const Modal = () => {
                 alt={"Give a suggest to NEWLEKA"}
                 className="w-full max-w-[210px] h-auto"
               />
-              <span id="suggestNewleka" className="mt-6">
+              <span id="suggestNewleka" className="mt-6 break-keep">
                 뉴레카에 대해 제안하기
               </span>
             </div>
           </button>
-          <button>
+          <button onClick={handleCardClick}>
             <div
-              id="suggestNewleka"
-              class="flex justify-center items-center flex-col w-[250px] h-[336px] bg-white rounded-lg p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+              id="suggestNewsletter"
+              class={
+                isB
+                  ? "bg-primary-100 border-2 border-primary-450 flex justify-center items-center flex-col w-[30vw] lg:w-[250px] h-[336px] rounded-lg lg:p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+                  : "flex justify-center items-center flex-col  w-[30vw] lg:w-[250px] h-[336px] bg-white rounded-lg lg:p-5 m-[14px] shadow-shadow-1 hover:shadow-none"
+              }
             >
               <Image
-                id="suggestNewleka"
+                id="suggestNewsletter"
                 src={suggestNLImg}
                 alt={"Give a suggest to NEWLEKA"}
                 className="w-full max-w-[210px] h-auto"
               />
-              <span id="suggestNewleka" className="mt-6">
+              <span id="suggestNewsletter" className="mt-6 break-keep">
                 새로운 뉴스레터 알려주기
               </span>
             </div>
           </button>
         </div>
         <div className="flex justify-end">
-          <Btn content={"취소"} name={"cancel"} />
+          <Btn content={"취소"} name={"cancel"} onClick={handleClose} />
           <Btn
-            content={"제안하기"}
-            name={"suggest-link"}
-            link={"https://forms.gle/S8rcCoCvxAYA6YbGA"}
+            content={isA ? "제안하기" : isB ? "알려주기" : "제안하기"}
+            name={isA || isB ? "suggest-link" : "suggest-disable"}
+            link={
+              isA
+                ? "https://forms.gle/S8rcCoCvxAYA6YbGA"
+                : isB
+                ? "https://forms.gle/jYrE9TeSnXEnfk7S6"
+                : ""
+            }
           />
         </div>
       </div>
     </div>
-  );
+  ) : null;
 
   if (isBrowser) {
     return ReactDOM.createPortal(
--- components/NavBtn.jsx
+++ components/NavBtn.jsx
@@ -1,14 +1,20 @@
 "use client";
 
 import Modal from "@components/Modal";
+import { useState } from "react";
 
 const NavBtn = ({ content }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <button className="py-2 px-4 ml-[10px] bg-primary-450 active:bg-primary-500 cursor-pointer text-white rounded-full text-sm">
+      <button
+        className="py-2 px-4 ml-[10px] bg-primary-450 active:bg-primary-500 cursor-pointer text-white rounded-full text-sm"
+        onClick={() => setShowModal(true)}
+      >
         {content}
       </button>
-      <Modal />
+      <Modal showModal={showModal} onClose={() => setShowModal(false)}/>
     </>
   );
 };
