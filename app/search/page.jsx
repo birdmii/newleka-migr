4160c2ecc91df64d3e46840bd772f7306b28e555
Saemi Jung <jung.sammie@gmail.com>
2022-11-22 16:07:07 +1300

    Fix search page error

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,6 +1,8 @@
 import { supabase } from "@utils/database";
 import Cards from "@components/Cards";
 
+export const revalidate = 0
+
 async function getNewslettersBySearchKeyword(keyword) {
   const { data, error } = await supabase.rpc("get_search_results", {
     qs: keyword,
