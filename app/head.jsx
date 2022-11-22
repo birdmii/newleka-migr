7671acbb222257b44c160011e10e22a8603ecb6e
Saemi Jung <jung.sammie@gmail.com>
2022-11-22 14:40:13 +1300

    Add title, keywords, description, og-image

--- app/head.jsx
+++ app/head.jsx
@@ -1,10 +1,35 @@
-export default function Head() {
+const Head = ({ title, keywords, description }) => {
   return (
     <>
-      <title></title>
+      <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/assets/favicon.ico" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://new-le-ka.vercel.app/" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/assets/metadata-img.png" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://new-le-ka.vercel.app/" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="/assets/metadata-img.png"></meta>
     </>
-  )
-}
+  );
+};
+
+Head.defaultProps = {
+  title: "NEW・LE・KA | 새로운 뉴스레터를 찾아보세요!",
+  keywords: "newsletter,뉴스레터,뉴레카,newleka",
+  description:
+    "새로운 뉴스레터를 찾고 계신가요? NEW・LE・KA(뉴레카)에서 원하는 뉴스레터를 손쉽게 찾아보세요!",
+};
+
+export default Head;
